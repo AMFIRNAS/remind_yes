423084ad81773cdf7617e3e840eaf9e799455bbb
Firnas Aleem <firnas.aleem@ifsworld.com>
2020-05-03 04:15:11 +0530

    add selection sort and some corrections

--- sorting_algorithms/bubble_sort/BubbleSort.java
+++ sorting_algorithms/bubble_sort/BubbleSort.java
@@ -4,8 +4,8 @@ public int[] bubbleSort(int[] list){
 	
 	
 	int i,j,temp = 0;
-	for (int i=0; i<list.length-1; i++){
-		for (int j=0; j<list.length-1-i; j++){
+	for (i=0; i<list.length-1; i++){
+		for (j=0; j<list.length-1-i; j++){
 			if(list[j] > list[j+1]){
 				temp = list[j+1];
 				list[j+1] = list[j];
---
+++ sorting_algorithms/bubble_sort/SelectionSort.java
@@ -0,0 +1,23 @@
+public int[] selectionSort(int list[]){
+	
+	int i,j,minValue,minIndex,temp = 0;
+	for(i = 0; i<list.length; i++){ // i< list.length-1 ??
+		minValue = list[i];
+		minIndex = i;
+		
+		for (j=i; j<list.length; j++){
+			if (list[j] < minValue){
+				minValue = list[j];
+				minIndex = j;
+			}
+		}
+		
+		if (minValue < list[i]){
+			temp = list[i];
+			list[i] = list[minIndex];
+			list[minIndex] = temp;
+		}
+		
+	}
+	
+}
